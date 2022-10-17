f3623253705b8c7bf2cee0f41c3846f8a5da69c6
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-17 22:10:50 +0500

    Изменнено условие цикла  while

--- homeWork_exit/Program.cs
+++ homeWork_exit/Program.cs
@@ -28,7 +28,7 @@ namespace homeWork_exit
                 $"{CommandHeal} - излечиться на {healPalyer} hp\n" +
                 $"{CommandExit} - убежать");
 
-            while (hpDragon >= 0) 
+            while (isExit == true) 
             {
                 inputUserText = Console.ReadLine();
 
@@ -38,25 +38,22 @@ namespace homeWork_exit
                         hpDragon -= damagePalyer;
                         Console.WriteLine($"Вы нанесли {damagePalyer} теперь у дракона {hpDragon} hp");
                         break;
+
                     case CommandHeal:
                         hpPlayer += healPalyer;
                         Console.WriteLine($"Вы исцелили себя на { healPalyer} теперь у вас {hpPlayer} hp");
                         break;
+
                     case CommandExit:
                         Console.WriteLine("Вы трус");
                         isExit = true;
                         break;
+
                     default:
                         Console.WriteLine("Вы пытаетесь что-то сделать, но поитогу ударяете сами себя");
                         hpPlayer -= damagePalyer;
                         Console.WriteLine($"Теперь у вас {hpPlayer} hp");
                         break;
-                }
-
-                if (isExit == true) 
-                {
-                    Console.ReadKey();
-                    break;
                 }
 
                 Console.WriteLine($"Дракон нанес вам {damageDragon} урона.");
@@ -67,6 +64,12 @@ namespace homeWork_exit
                 {
                     Console.WriteLine("О чудо ангелы помогают вам, и исцеляют вас.");
                     hpPlayer = 100;
+                }
+
+                if (hpDragon <= 0)
+                {
+                    Console.WriteLine("Почему ангелы исцеляют дракона???");
+                    hpDragon = 1000;
                 }
 
                 Console.WriteLine("Что вы делете дальше? ");
